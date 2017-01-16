a5c540f3dd649423dca8ffc3eebfa7cbcf22cec4
Jochem <paranormalpirate111@gmail.com>
2017-01-17 00:40:32 +0100

    added protected fillable field to keuken

--- app/Keuken.php
+++ app/Keuken.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Keuken extends Model
 {
 	protected $table = 'keuken';
+	protected $fillable = [
+        'keuken'
+    ];
     public function recept()
     {
     	return $this->hasMany(Recept::class);
