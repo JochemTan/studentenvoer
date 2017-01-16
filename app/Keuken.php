680b136c128225748f13be895c6d8ccdd573580a
Jochem <paranormalpirate111@gmail.com>
2017-01-17 00:50:07 +0100

    changed table name

--- app/Keuken.php
+++ app/Keuken.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Keuken extends Model
 {
-	protected $table = 'keuken';
-	protected $fillable = [
-        'keuken'
-    ];
+	protected $table = 'Keuken';
+	// protected $fillable = [
+ //        'keuken'
+ //    ];
     public function recept()
     {
     	return $this->hasMany(Recept::class);
